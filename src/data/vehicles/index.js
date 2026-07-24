8acdac090c7378d05fa40476edfb9dac4d4133bd
Luckyillia <zajchenkoi2007@gmail.com>
2026-07-24 16:06:43 +0200

    Add new vehicles vaz 21011

--- src/data/vehicles/index.js
+++ src/data/vehicles/index.js
@@ -1,6 +1,7 @@
 //Legkovoy
 import volkswagenJetta from "./legkovoy/volkswagen-jetta";
 import vaz2108 from "./legkovoy/vaz-2108";
+import vaz21011 from "./legkovoy/vaz-21011";
 
 // Sobytiya
 import bmw850CsiKoenigSpecialsKs8 from "./sobytiya/bmw-850-csi-koenig-specials-ks8";
@@ -10,6 +11,7 @@ import vaz2108Buran from "./sobytiya/vaz-2108-buran";
 export { categories, getCategory } from "../categories";
 
 export const vehicles = [
+  vaz21011,
   vaz2108,
   vaz2108Buran,
   bmw850CsiKoenigSpecialsKs8,
--- src/data/vehicles/legkovoy/vaz-21011.js
+++ src/data/vehicles/legkovoy/vaz-21011.js
@@ -1,13 +1,13 @@
 export default {
   slug: "vaz-21011",
   category: "legkovoy",
-  name: "ВАЗ-21011",
-  image: "",
+  name: "ВАЗ 21011",
+  image: "/images/vehicles/legkovoy/vaz-21011/main.jpg",
   colors: [
-    { id: "white", name: "Белый", hex: "#E7E9EA", image: "" },
-    { id: "green", name: "Изумруд", hex: "#3E6B4F", image: "" },
-    { id: "black", name: "Чёрный лак", hex: "#1A1B1D", image: "" },
-    { id: "graphite", name: "Графит", hex: "#4B4E52", image: "" },
-    { id: "gold", name: "Золотистый", hex: "#B99A4E", image: "" },
+    { id: "zelenaya-lipa-325", name: "Зеленая липа(325)", hexes: ["#4F7A2B", "#000000"], image: "/images/vehicles/legkovoy/vaz-21011/zelenaya-lipa-325.jpg" },
+    { id: "adriatika-425", name: "Адриатика(425)", hexes: ["#2B80AC", "#000000"], image: "/images/vehicles/legkovoy/vaz-21011/adriatika-425.jpg" },
+    { id: "korrida-165", name: "Коррида(165)", hexes: ["#972300", "#0E0402"], image: "/images/vehicles/legkovoy/vaz-21011/korrida-165.jpg" },
+    { id: "olivkovyy-340", name: "Оливковый(340)", hexes: ["#405326", "#000000"], image: "/images/vehicles/legkovoy/vaz-21011/olivkovyy-340.jpg" },
+    { id: "ohra-zolotistaya-208", name: "Охра золотистая(208)", hexes: ["#AB4E14", "#000000"], image: "/images/vehicles/legkovoy/vaz-21011/ohra-zolotistaya-208.jpg" },
   ],
 };
