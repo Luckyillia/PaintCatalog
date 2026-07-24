04c75b42f38c6791f44ba3f2b6cba090b1e016c3
Luckyillia <zajchenkoi2007@gmail.com>
2026-07-24 19:24:07 +0200

    Add new vehicles bmw x6m f96

--- src/data/vehicles/index.js
+++ src/data/vehicles/index.js
@@ -2,6 +2,7 @@
 import volkswagenJetta from "./legkovoy/volkswagen-jetta";
 import vaz2108 from "./legkovoy/vaz-2108";
 import vaz21011 from "./legkovoy/vaz-21011";
+import bmwX6mF96 from "./legkovoy/bmw-x6m-f96";
 
 // Sobytiya
 import bmw850CsiKoenigSpecialsKs8 from "./sobytiya/bmw-850-csi-koenig-specials-ks8";
@@ -11,6 +12,7 @@ import vaz2108Buran from "./sobytiya/vaz-2108-buran";
 export { categories, getCategory } from "../categories";
 
 export const vehicles = [
+  bmwX6mF96,
   vaz21011,
   vaz2108,
   vaz2108Buran,
---
+++ src/data/vehicles/legkovoy/bmw-x6m-f96.js
@@ -0,0 +1,12 @@
+export default {
+  slug: "bmw-x6m-f96",
+  category: "legkovoy",
+  name: "BMW X6M (F96)",
+  image: "/images/vehicles/legkovoy/bmw-x6m-f96/main.jpg",
+  colors: [
+    { id: "color", name: "", hexes: ["#6E1207", "#000000"], image: "/images/vehicles/legkovoy/bmw-x6m-f96/color.jpg" },
+    { id: "color", name: "", hexes: ["#0B1120", "#020307"], image: "/images/vehicles/legkovoy/bmw-x6m-f96/color.jpg" },
+    { id: "color", name: "", hexes: ["#431410", "#0B0101"], image: "/images/vehicles/legkovoy/bmw-x6m-f96/color.jpg" },
+    { id: "color", name: "", hexes: ["#333325", "#0E0C06"], image: "/images/vehicles/legkovoy/bmw-x6m-f96/color.jpg" },
+  ],
+};
