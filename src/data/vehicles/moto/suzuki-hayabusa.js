6475d3a0af1b14726319cf118464072914ae1fcd
Luckyillia <zajchenkoi2007@gmail.com>
2026-08-18 17:57:42 +0200

    Update color on suzuki accentHex

--- src/data/vehicles/moto/suzuki-hayabusa.js
+++ src/data/vehicles/moto/suzuki-hayabusa.js
@@ -5,7 +5,7 @@ export default {
   tags: ["suzuki", "moto", "inomarka", "aziatskiy", "yaponskiy", "era-2020s", "rwd"],
   image: "/images/vehicles/moto/suzuki-hayabusa/main.jpg",
   colors: [
-    { id: "metallic-thunder-gray-candy-daring-red", name: "Metallic Thunder Gray + Candy Daring Red", hexes: ["#252627", "#000000"], accentHex: "#B23A2E", image: "/images/vehicles/moto/suzuki-hayabusa/metallic-thunder-gray-candy-daring-red.jpg" },
-    { id: "pearl-vigor-blue-pearl-brilliant-white", name: "Pearl Vigor Blue + Pearl Brilliant White", hexes: ["#B8B6B6", "#000000"], accentHex: "#B23A2E", image: "/images/vehicles/moto/suzuki-hayabusa/pearl-vigor-blue-pearl-brilliant-white.jpg" },
+    { id: "metallic-thunder-gray-candy-daring-red", name: "Metallic Thunder Gray + Candy Daring Red", hexes: ["#252627", "#000000"], accentHex: "#7B0B03", image: "/images/vehicles/moto/suzuki-hayabusa/metallic-thunder-gray-candy-daring-red.jpg" },
+    { id: "pearl-vigor-blue-pearl-brilliant-white", name: "Pearl Vigor Blue + Pearl Brilliant White", hexes: ["#B8B6B6", "#000000"], accentHex: "#183666", image: "/images/vehicles/moto/suzuki-hayabusa/pearl-vigor-blue-pearl-brilliant-white.jpg" },
   ],
 };
